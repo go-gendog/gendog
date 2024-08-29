68617157df50e01fe3090c3155507dc730291a8c
Alex Holmes <alex@sed.dev>
2024-08-29 00:48:39 -0400

    Learning text/template

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Gendog
-A tool for YAML developers to generate Godog boilerplate API tests for your http server from an OpenAPI 3 specification.
-The use of this tool is intended to be paired with [open-api-codegen/open-api-codegen](https://github.com/oapi-codegen/oapi-codegen/?tab=readme-ov-file),
-but can be used for any Go project.
+A tool for YAML developers to generate Godog boilerplate API tests for your http an 
+OpenAPI 3 specification. The use of this tool is intended to be paired with 
+[open-api-codegen/open-api-codegen](https://github.com/oapi-codegen/oapi-codegen/?tab=readme-ov-file), but can be used for any Go project.
 
-_\[Developers\] will do anything to avoid the responsibility of writing tests. It's me, I'm \[developers\]._
+_\[Developers\] will do anything to avoid the responsibility of writing tests.
+It's me, I'm \[developers\]._
 
 ## Installation
 
--- main.go
+++ main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"text/template"
+)
 
 func main() {
-    fmt.Println("gendog")
+	// Open the templates folder
+	tmpl := "templates/"
+	dirEntries, err := os.ReadDir(tmpl)
+	if err != nil {
+		log.Fatalf("Failed to read directory: %v", err)
+	}
+
+	// Read all files into a []string
+	var templates []string
+	for _, entry := range dirEntries {
+		if !entry.IsDir() {
+			file, err := os.Open(filepath.Join(tmpl, entry.Name()))
+			if err != nil {
+				log.Fatalf("Failed to open file %s: %v", entry.Name(), err)
+			}
+			defer func(file *os.File) {
+				_ = file.Close()
+			}(file)
+
+			content, err := io.ReadAll(file)
+			if err != nil {
+				log.Fatalf("Failed to read file %s: %v", entry.Name(), err)
+			}
+
+			templates = append(templates, string(content))
+		}
+	}
+
+	// Create a new text/template with each string and execute with an example map
+	data := map[string]string{"Name": "bob"}
+	for _, tmplStr := range templates {
+		tmpl, err := template.New("example").Parse(tmplStr)
+		if err != nil {
+			log.Fatalf("Failed to parse template: %v", err)
+		}
+		err = tmpl.Execute(os.Stdout, data)
+		if err != nil {
+			log.Fatalf("Failed to execute template: %v", err)
+		}
+		fmt.Println()
+	}
 }
